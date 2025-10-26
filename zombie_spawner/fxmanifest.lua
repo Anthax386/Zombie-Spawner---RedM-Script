6342cfb169062417e6d7f963f73686a9c1efab87
Anthax386 <kevinanthax@protonmail.com>
2025-10-26 21:34:29 +0100

    add rdr3_warning

--- zombie_spawner/fxmanifest.lua
+++ zombie_spawner/fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'cerulean'
-game 'rdr2'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources will become incompatible once RedM ships.'
 
 author 'MrAnthax'
 description 'Permet de spawn des zombies dans RedM'
